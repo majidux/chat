ee2ae540b9cf26a559c87d88a25c9343c4e81984
m.darvishnejad <majid>
2019-02-13 10:55:57 +0330

    Delete useless codes

--- src/services/actions.js
+++ src/services/actions.js
@@ -1,5 +1,5 @@
 import {FETCH_USER,RECEIVE_ERROR,RECEIVE_POST} from "./type";
-import store from "../Pages/Home";
+// import store from "../Pages/Home";
 
 export const fetchUser = () => ({
     type: FETCH_USER
@@ -16,30 +16,15 @@ export const receiveError = error => ({
 });
 
 
-// export const thunk_action_creator = username => {
-//     const user = username.replace(/\s/g,"");
-//     store.dispatch(fetchUser());
-//     return function (dispatch,getState) {
-//         return fetch(`https://randomuser.me/api/`)
-//             .then(data => data.json())
-//             .then(data => {
-//                 dispatch(receivePost(data));
-//             })
-//             .catch(error=>dispatch(receiveError()));
-//     }
-// };
-
-
 export const fetchData = () => {
-    return (dispatch,getState) => {
-        console.warn('hello');
+    return (dispatch) => {
         dispatch(fetchUser());
         fetch(`https://randomuser.me/api?results=15`)
             .then(response => response.json())
             .then(data => {
                 dispatch(receivePost(data));
-                console.warn(data)
             })
             .catch(error => dispatch(receiveError(error)));
     }
 };
+
